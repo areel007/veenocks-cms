4ec7ada3044f68d16a1675e5d7e5c30b441a3540
areel007 <morenikejisunday@gmail.com>
2024-09-04 17:42:56 -0700

    fix cor issue

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
