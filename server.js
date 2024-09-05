261f230230777a85eb8b0d4a57ed0fd2de6c9332
areel007 <morenikejisunday@gmail.com>
2024-09-04 18:22:56 -0700

    fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { app } from "./app.js";
 import { connectToDatabase } from "./src/services/db.con.js";
 
 const PORT = process.env.PORT || 4141;
-const db = process.env.DB || "mongodb://127.0.0.1:27017/veenocks";
+const db = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/veenocks";
 
 connectToDatabase(db);
 
