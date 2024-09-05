03a6684436abe297d5793acd4e5ce4fd511ff31e
areel007 <morenikejisunday@gmail.com>
2024-09-04 18:40:58 -0700

    minor fix

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
+
 app.options("*", cors());
 
 if (process.env.NODE_ENV === "development") {
